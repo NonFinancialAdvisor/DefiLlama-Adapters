8283dcfab6238d617954b7b1c9540a76512dd534
NonFinancialAdvisor <155776586+NonFinancialAdvisor@users.noreply.github.com>
2024-03-15 17:48:13 +0100

    Add Arbitrum chain
    Add Arbitrum chain in config and create hallmark

--- projects/peapods-finance/index.js
+++ projects/peapods-finance/index.js
@@ -1,8 +1,15 @@
 const { sumTokens2 } = require("../helper/unwrapLPs");
 
 const config = {
-  ethereum: { indexManager: "0x0Bb39ba2eE60f825348676f9a87B7CD1e3B4AE6B", peasToken: "0x02f92800F57BCD74066F5709F1Daa1A4302Df875" }
-}
+  ethereum: {
+    indexManager: "0x0Bb39ba2eE60f825348676f9a87B7CD1e3B4AE6B",
+    peasToken: "0x02f92800F57BCD74066F5709F1Daa1A4302Df875",
+  },
+  arbitrum: {
+    indexManager: "0x0Bb39ba2eE60f825348676f9a87B7CD1e3B4AE6B",
+    peasToken: "0x02f92800F57BCD74066F5709F1Daa1A4302Df875",
+  },
+};
 
 const indexManagerABI =
   "function allIndexes() view returns (tuple(address index, bool verified)[])";
@@ -12,39 +19,57 @@ const indexABIassets =
 const stakingPoolABI = { stakingToken: "address:stakingToken" };
 
 const getTvl = async (api, isStaking) => {
-  const { indexManager, peasToken } = config[api.chain]
-  const ownerTokens = []
-  const indexes = (await api.call({ abi: indexManagerABI, target: indexManager, })).map(i =>i.index);
+  const { indexManager, peasToken } = config[api.chain];
+  const ownerTokens = [];
+  const indexes = (
+    await api.call({ abi: indexManagerABI, target: indexManager })
+  ).map((i) => i.index);
 
   //get staking pool contract for index (spp)
-  const stakingPools = await api.multiCall({ abi: indexABI.lpStakingPool, calls: indexes, });
+  const stakingPools = await api.multiCall({
+    abi: indexABI.lpStakingPool,
+    calls: indexes,
+  });
 
   //get assets this index consists of
-  const assetsResult = await api.multiCall({ abi: indexABIassets, calls: indexes, });
-  const stakingTokens = await api.multiCall({ abi: stakingPoolABI.stakingToken, calls: stakingPools, });
+  const assetsResult = await api.multiCall({
+    abi: indexABIassets,
+    calls: indexes,
+  });
+  const stakingTokens = await api.multiCall({
+    abi: stakingPoolABI.stakingToken,
+    calls: stakingPools,
+  });
 
   assetsResult.forEach((assets, i) => {
-    ownerTokens.push([assets.map(i => i.token), indexes[i]])
-  })
-  stakingTokens.forEach((stakingToken, i) => ownerTokens.push([[stakingToken], stakingPools[i]]))
-  await sumTokens2({ api, ownerTokens, blacklistedTokens: indexes, resolveLP: true, })
-  indexes.forEach(i => api.removeTokenBalance(i))
-  Object.keys(api.getBalances()).forEach(token => {
-    let remove = (new RegExp(peasToken, "i")).test(token)
-    if (isStaking) remove = !remove
-    if (remove)
-      api.removeTokenBalance(token)
-  })
-  return api.getBalances()
+    ownerTokens.push([assets.map((i) => i.token), indexes[i]]);
+  });
+  stakingTokens.forEach((stakingToken, i) =>
+    ownerTokens.push([[stakingToken], stakingPools[i]]),
+  );
+  await sumTokens2({
+    api,
+    ownerTokens,
+    blacklistedTokens: indexes,
+    resolveLP: true,
+  });
+  indexes.forEach((i) => api.removeTokenBalance(i));
+  Object.keys(api.getBalances()).forEach((token) => {
+    let remove = new RegExp(peasToken, "i").test(token);
+    if (isStaking) remove = !remove;
+    if (remove) api.removeTokenBalance(token);
+  });
+  return api.getBalances();
 };
 
 module.exports = {
   methodology: "Aggregates TVL in all Peapods Finance indexes created",
-}
+  hallmarks: [[1710444951, "Arbitrum launch"]],
+};
 
-Object.keys(config).forEach(chain => {
+Object.keys(config).forEach((chain) => {
   module.exports[chain] = {
-    tvl: async (_, _b, _cb, { api, }) => getTvl(api, false),
-    staking: async (_, _b, _cb, { api, }) => getTvl(api, true),
-  }
-})
+    tvl: async (_, _b, _cb, { api }) => getTvl(api, false),
+    staking: async (_, _b, _cb, { api }) => getTvl(api, true),
+  };
+});
